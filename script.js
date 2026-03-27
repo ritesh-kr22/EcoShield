3b64ccceaa9560161fe2325d83c67ba59adbe502
ritesh kumar <166797186+ritesh-kr22@users.noreply.github.com>
2026-03-28 03:38:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// gsap.to("#bo",{
+//     // backgroundColor:"#ff0000",
+//     borderRadius: 10,
+//     duration:0.1,
+//     y:100,
+//     scrollTrigger:{
+//         trigger:"#bo",
+//         scroller:"body",
+//         markers: true,
+//         start:"top 10%",
+//         scrub:1
+//     }
+// })
